2a1b600efa4ce1b64b9943f706f345a7b769919f
Vimalan S <vimalansenthil@gmail.com>
2024-03-13 14:02:55 +0530

    Navbar to Home Navigation Fixed

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -13,25 +13,23 @@ const Navbar = () => (
             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
           </label>
           <ul tabIndex={0} className="menu dropdown-content mt-3 z-[5] p-2 bg-primary-black shadow rounded-box w-52">
-            <li><a className="hover:text-black" href="/">Home</a></li>
-            <li><a className="hover:text-black" href="/about">About</a></li>
-            <li><a className="hover:text-black" href="/events">Event Registration</a></li>
-            <li><a className="hover:text-black" href="/regprofile">Create Profile</a></li>
-            <li><a className="hover:text-black" href="/viewprofile">My Profile</a></li>
-            <li><a className="hover:text-black" href="/signup">Signup/Login</a></li>
-            <li><a className="hover:text-black" href="/guidelines">Guidelines</a></li>
-          </ul>
+          <li><a className="hover:text-black" href="/" key="home">Home</a></li>
+          <li><a className="hover:text-black" href="/about" key="about">About</a></li>
+          <li><a className="hover:text-black" href="/events" key="events">Event Registration</a></li>
+          <li><a className="hover:text-black" href="/regprofile" key="regprofile">Create Profile</a></li>
+          <li><a className="hover:text-black" href="/viewprofile" key="viewprofile">My Profile</a></li>
+          <li><a className="hover:text-black" href="/signup" key="signup">Signup/Login</a></li>
+          <li><a className="hover:text-black" href="/guidelines" key="guidelines">Guidelines</a></li>
+        </ul>
         </div>
       </div>
-      <div class="navbar-center">    
+      <div class="navbar-center" >    
         <img src="ptu-logo.png" class="h-20 w-22 max-w-full pr-4" alt="PTU Logo" />
-        <a className="btn btn-ghost text-xl font-bold font-title hidden lg:block">PUDUCHERRY TECHNOLOGICAL UNIVERSITY</a>
+        <a className="btn btn-ghost text-xl font-bold font-title hidden lg:block z-0" href="/" >PUDUCHERRY TECHNOLOGICAL UNIVERSITY</a>
         <a class="btn btn-ghost text-xl font-bold font-title lg:hidden">PTU</a>
       </div>
       <div className="navbar-end">
-        <button className="btn btn-ghost btn-circle !no-underline">
-          <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
-        </button>
+        
         <button className="btn btn-ghost btn-circle !no-underline">
           <div className="indicator">
             <a href="https://www.instagram.com/rejouir_ptu?igsh=dGhkYWxzZTFpZ3N2">
